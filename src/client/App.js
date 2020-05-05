a915c7131e37dbce79e3351f31c931987a3feae1
Efim Pushkarev <pushkarevev@yandex.ru>
2020-05-05 10:26:39 +0300

    Initial commit

---
+++ src/client/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import InputForm from './InputForm';
+import InfoForm from './InfoForm';
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
